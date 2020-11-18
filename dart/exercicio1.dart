24ead5e803bfec2f9566c914ce89f0e123164306
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-18 17:25:44 -0300

    exercício1

---
+++ dart/exercicio1.dart
@@ -0,0 +1,22 @@
+//Perguntar a idade da pessoa;
+//se a idade for maior ou igual a 18
+//  informar que é maior de idade;
+//se não for
+//  ele não é maior de idade
+
+main(){
+
+  var idade;
+
+  print("Qual sua idade?");
+  idade = 27;
+
+  if(idade >= 18){
+    print("É maior de idade");
+    
+  }
+  else {
+    print("É menor de idade");
+
+  }
+}
