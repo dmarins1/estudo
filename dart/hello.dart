4527c76d2a917c6e91065852d50331b850482906
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-18 16:27:49 -0300

    Variáveis

--- dart/hello.dart
+++ dart/hello.dart
@@ -1,8 +1,13 @@
 main(){
 
-  print("Hello World!");
-  print(25);
-  print(3.14);
-  print([12,13,14,15]);
+  // print("Hello World!");
+  // print(25);
+  // print(3.14);
+  // print([12,13,14,15]);
+
+  var hello = "Hello World";
+  hello = "Reatribuido";
+  print(hello);
+   
 
 }
