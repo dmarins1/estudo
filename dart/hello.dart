f96221e1e3dfe9ce05c638731b2df9fe0188f887
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-18 17:07:52 -0300

    Operações  numéricas

--- dart/hello.dart
+++ dart/hello.dart
@@ -1,13 +1,22 @@
 main(){
 
-  // print("Hello World!");
-  // print(25);
-  // print(3.14);
-  // print([12,13,14,15]);
+  // ------------------------------------------------------------- //
+  // Variáveis
 
   var hello = "Hello World";
   hello = "Reatribuido";
   print(hello);
    
+
+  // ------------------------------------------------------------- //
+  // Operações Matemáricas
+
+  var num1 = 5;
+  var num2 = 3;
+  var total = num1 * num2 ;
+
+  var total2 = (num1 + num2) + (num1 / num2);
+
+  print(total2);
 
 }
