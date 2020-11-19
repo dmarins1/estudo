e1c03c0165dd7ec1c4aa1cfddb778d9aea64b766
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-19 17:15:40 -0300

    Calculo IMC

---
+++ dart/calculo_imc.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+// Pegar peso
+// Pegar Altura
+// Realizar Calculo
+// Retornar IMC
+
+main(){
+
+  print("Informe seu peso: ");
+  var peso = int.parse(stdin.readLineSync());
+  
+  print("Informe sua altura em cm: ");
+  var altura = double.parse(stdin.readLineSync());
+
+  var calculo = peso / (altura * altura);
+
+  print("Calculo do IMC: " + calculo.toString());
+
+  if(calculo <= 18.5){
+    print("Abaixo do Peso");
+  }
+
+  else if (calculo <= 24.9){
+    print("Peso normal");
+  }
+
+  else if (calculo <= 29.9){
+    print("Sobrepeso");
+  }
+
+  else if (calculo <= 34.9){
+    print("Obesidade Grau 1");
+  } 
+
+  else if (calculo <= 39.9){
+    print("Obresidade Grau 2");
+  }
+  
+  else{
+    print("Obesidade Grau 3");
+  }
+
+
+
+
+}
