bed8c9502560d2d4028529b7dac2a15e87ac1edf
dmarins1 <d.marins@hotmail.com>
2020-11-20 12:35:08 -0300

    Função com retorno

--- dart/aulas/funcoes.dart
+++ dart/aulas/funcoes.dart
@@ -1,17 +1,12 @@
 import 'dart:io';
 
-// Pegar peso
-// Pegar Altura
-// Realizar Calculo
-// Retornar IMC
-
 main(){
 
-calculoIMC();
+pegaDadosImc();
 
 }
 
-calculoIMC(){
+pegaDadosImc(){
 
   print("Informe seu peso: ");
   int peso = int.parse(stdin.readLineSync());
@@ -19,33 +14,39 @@ calculoIMC(){
   print("Informe sua altura: ");
   double altura = double.parse(stdin.readLineSync());
 
-  double resultado_imc = peso / (altura * altura);
+  double calc = calculo(peso, altura);
 
-  print("Calculo do IMC: " + resultado_imc.toString());
+  print("Seu calculo IMC: " + calc.toString());
 
-  imprimirResultado(resultado_imc);
+  imprimirResultado(calc);
  
 }
 
-imprimirResultado(double resultado){
+double calculo(int peso, double altura){
 
- if(resultado <= 18.5){
+  return peso / (altura * altura);
+
+}
+
+imprimirResultado(double imc){
+
+ if(imc <= 18.5){
     print("Abaixo do Peso");
   }
 
-  else if (resultado <= 24.9){
+  else if (imc <= 24.9){
     print("Peso normal");
   }
 
-  else if (resultado <= 29.9){
+  else if (imc <= 29.9){
     print("Sobrepeso");
   }
 
-  else if (resultado <= 34.9){
+  else if (imc <= 34.9){
     print("Obesidade Grau 1");
   } 
 
-  else if (resultado <= 39.9){
+  else if (imc <= 39.9){
     print("Obresidade Grau 2");
   }
   
