5d8ad4b0fa825ce76473f37aa0e1a17282530ba8
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-25 17:56:08 -0300

    Manipulação de array

--- dart/aulas/arrays.dart
+++ dart/aulas/arrays.dart
@@ -2,29 +2,44 @@ import 'dart:io';
 
 main(){
 
-  var text = [];
+  var text = ["Douglas Marins", "Paloma Marins", "Thaianne Faria", "Raoni Faria"];
 
-  bool condicao = true;
+  // bool condicao = true;
   
-  while(condicao){
-    print("Digite um nome: ");
-    String nome = stdin.readLineSync();
+  // while(condicao){
+  //   print("Digite um nome: ");
+  //   String nome = stdin.readLineSync();
 
-    if(nome == 'sair'){
+  //   if(nome == 'sair'){
 
-      print("Programa Finalizado");
-      condicao = false;
-    } else{
+  //     print("Programa Finalizado");
+  //     condicao = false;
+  //   } else{
 
-      text.add(nome);
+  //     text.add(nome);
 
-    }
+  //   }
 
-    print("$text\n");
+  //   print("$text\n");
 
-  }
+  // }
+
+  //array.add - para adicionar dados ao array
+  text.add("Famila");
+
+  print(text);
 
   //array.leghth - para pegar o tramanho do array
   print("Existem " +  text.length.toString() + " dados no array");
 
+  // printando o elemento do array
+  print(text[0]);
+
+  //array.removeAt - para remover um item do array pelo index
+  text.removeAt(0);
+
+  //array.remove - para remover um item do array pelo valor
+  text.remove("Raoni Faria");
+
+  print(text);
 }
