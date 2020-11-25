36877296c4c6a7bc7d28cff741e742209593220e
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-25 13:40:22 -0300

    Tipagem

---
+++ dart/aulas/tipagem.dart
@@ -0,0 +1,15 @@
+main(){
+
+  // Numeros inteiros
+  int num1 = 1;
+  
+  // Numeros Flutuantes
+  double num2 = 3.1;
+
+  // Texto
+  String text = "Douglas Marins";
+
+  // Boleanos
+  bool isTrue = false;
+
+}
