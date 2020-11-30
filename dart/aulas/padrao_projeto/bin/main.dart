300921218aaa19c90cbcd2ba0d678bc2af324103
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-30 14:55:59 -0300

    Estrutura de decisão com argumentos

--- dart/aulas/padrao_projeto/bin/main.dart
+++ dart/aulas/padrao_projeto/bin/main.dart
@@ -1,3 +1,7 @@
 void main(List<String> arguments) {
-  print(arguments);
+  if (arguments[0] == 'hackear') {
+    print('Hackeamento concluido');
+  } else if (arguments[0] == 'imprimir') {
+    print('executando impressão');
+  }
 }
