afb6c040e533a8ab385d9c85171ab6f17b3d5015
Douglas Marins <douglas.marins@sectrans.com.br>
2020-11-30 13:42:04 -0300

    Maps

---
+++ dart/aulas/maps.dart
@@ -0,0 +1,16 @@
+main(){
+
+  // List <String> nomes = ["Douglas", "Thaianne", "Raoni", "Paloma"];
+  
+  Map <String, dynamic> maps = {
+    "nome": "Douglas",
+    "idade": 27,
+    "cidade": "Rio de Janeiro",
+    "estado": "RJ"
+  };
+
+  maps["nome"] = "Marins";
+  print(maps["nome"]);
+
+
+}
