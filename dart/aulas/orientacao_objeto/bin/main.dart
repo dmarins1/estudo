63ee81df61fe4c09813a90a2e44bcee8276202a1
Douglas Marins <douglas.marins@sectrans.com.br>
2020-12-01 17:29:17 -0300

    Construtor Inteligente

--- dart/aulas/orientacao_objeto/bin/main.dart
+++ dart/aulas/orientacao_objeto/bin/main.dart
@@ -1,1 +1,9 @@
-void main(List<String> arguments) {}
+import '../lib/pessoa.dart';
+
+void main(List<String> arguments) {
+  Pessoa pessoa = Pessoa(idade: 27, nome: 'Douglas', sexo: 'M');
+
+  print(pessoa.nome);
+  print(pessoa.idade);
+  print(pessoa.sexo);
+}
