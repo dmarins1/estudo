653745357388c4d21de2733c4f3c55e8c4561ec6
Douglas Marins <douglas.marins@sectrans.com.br>
2020-12-01 17:33:58 -0300

    Variáveis privadas

--- dart/aulas/orientacao_objeto/lib/pessoa.dart
+++ dart/aulas/orientacao_objeto/lib/pessoa.dart
@@ -3,5 +3,11 @@ class Pessoa {
   int idade;
   String sexo;
 
+  //Variavel Privada
+  String _nomelocal = "Conteudo";
+
+  // Variável que recebe dados apenas uma vez.
+  final String sobreNome = 'Marins';
+
   Pessoa({this.nome, this.idade, this.sexo});
 }
